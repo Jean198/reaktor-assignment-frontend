db9557955cfd67aae3b06222f5d81a081e7eb0e1
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-12-28 09:25:09 +0200

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import DronesTable from './components/dronesTable/DronesTable';
 
 function App() {
   return (
     <div className="App">
-      Reaktor frontend
+      <DronesTable/>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/components/dronesTable/DronesTable.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Drone from '../drone/Drone'
+
+const DronesTable = () => {
+  return (
+    <div>
+        <Drone/>
+    </div>
+  )
+}
+
+export default DronesTable
