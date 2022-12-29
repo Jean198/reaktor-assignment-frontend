139e1d2ac0ccfaa340f690d438725af850c85d78
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-12-29 09:47:33 +0200

    loader and styles

--- src/components/dronesTable/DronesTable.js
+++ src/components/dronesTable/DronesTable.js
@@ -2,21 +2,24 @@ import React, { useEffect, useState } from "react";
 import Drone from "../drone/Drone";
 import axios from "axios";
 import { URL } from "../../App";
-import birdnest from "../../assets/images/birdnest.png"
-import './dronesTable.css'
-
-
+import birdnest from "../../assets/images/birdnest.png";
+import loader from "../../assets/images/loader.gif";
+import "./dronesTable.css";
 
 const DronesTable = () => {
   const [dronesData, setDronesData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
+    setIsLoading(true);
     try {
       const interval = setInterval(async () => {
         await axios.get(`${URL}`).then((response) => {
+          setIsLoading(false);
           console.log(response.data);
           setDronesData(response.data);
         });
-      }, 3000);
+      }, 10000);
       return () => clearInterval(interval);
     } catch (error) {
       console.log(error.message);
@@ -28,6 +31,11 @@ const DronesTable = () => {
       <div>
         <img src={birdnest} alt="" className="birdnest-image" />
       </div>
+      {
+        <div className="loader">
+          {isLoading ? <img src={loader} alt="" /> : null}
+        </div>
+      }
       <div className="table-responsive">
         <table className=" table">
           <thead className="table-dark">
@@ -43,19 +51,13 @@ const DronesTable = () => {
           <tbody>
             {dronesData &&
               dronesData.map((drone, index) => {
-                return (
-                  <Drone drone={drone} index={index}/>
-                );
+                return <Drone drone={drone} index={index} />;
               })}
           </tbody>
         </table>
       </div>
     </>
   );
-}
+};
 
-export default DronesTable
-
-
-
-
+export default DronesTable;
